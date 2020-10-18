4f9874e9d1de769a3b9437220bc8b063728d2d07
berious <36975740+berious@users.noreply.github.com>
2020-10-18 19:31:02 +0900

    구분선 작업 및 노트 오브잭트 생성 및 이동

---
+++ New Unity Project (2)/Assets/Scrips/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //GameManager를 싱글 톤 처리
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    public float noteSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
